f764e90505fedf8bbc877c463a727d3362801607
Alejandra Blandon <alejandrablandon@hotmail.com>
2022-11-23 23:53:57 -0500

    Build of gradle

---
+++ src/test/java/stepsdefinition/ExploringPageStepsDefinition.java
@@ -0,0 +1,31 @@
+package stepsdefinition;
+
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class ExploringPageStepsDefinition {
+
+    @Given("User go through the page")
+    public void user_go_through_the_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("User scroll all page, section by section")
+    public void user_scroll_all_page_section_by_section() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("Fill out required information on Contact Sales section")
+    public void fill_out_required_information_on_contact_sales_section() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("The reporter is generated")
+    public void the_reporter_is_generated() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
---
+++ src/test/resources/features/exploring_page.feature
@@ -0,0 +1,7 @@
+Feature: User explore the Team International page in Chrome browser.
+
+  Scenario: User explore the page
+    Given User go through the page
+    When User scroll all page, section by section
+    And Fill out required information on Contact Sales section
+    Then The reporter is generated
